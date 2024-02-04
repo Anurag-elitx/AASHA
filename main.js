abef8a4dcd01df56f4f8451609313f0c85caf540
Akansha_chaurasia <akansha020btcse22@igdtuw.ac.in>
2024-02-04 18:06:40 +0530

    new website

--- main.js
+++ main.js
@@ -1,18 +1,17 @@
-const loginText = document.querySelector(".title-text .login");
-const loginForm = document.querySelector("form.login");
-const loginBtn = document.querySelector("label.login");
-const signupBtn = document.querySelector("label.signup");
-const signupLink = document.querySelector("form .signup-link a");
+const scrollBtns = document.querySelectorAll('.scroll-btn');
 
-signupBtn.onclick = (()=>{
-  loginForm.style.marginLeft = "-50%";
-  loginText.style.marginLeft = "-50%";
+scrollBtns.forEach(btn => {
+    btn.addEventListener('click', smoothScroll);
 });
-loginBtn.onclick = (()=>{
-  loginForm.style.marginLeft = "0%";
-  loginText.style.marginLeft = "0%";
-});
-signupLink.onclick = (()=>{
-  signupBtn.click();
-  return false;
-});
+
+function smoothScroll(e) {
+    e.preventDefault();
+
+    const targetId = this.getAttribute('data-target');
+    const targetSection = document.querySelector(targetId);
+
+    window.scrollTo({
+        top: targetSection.offsetTop,
+        behavior: 'smooth'
+    });
+}
